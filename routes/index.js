d8229f42d4cb94f05347ac602c08329d8bb301c7
Karmanya Veer Sharma <karmanyasharma5@gmail.com>
2020-10-14 23:22:40 +0530

    Added login page

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 /* GET login page. */
 router.get('/', function (req, res, next) {
-	res.locals.headerTitle = 'Login';
-	res.render('login');
+	res.render('login', {
+		layout:'login'
+	});
 });
 /* GET dashboard page. */
 router.get('/dashboard', function (req, res, next) {
